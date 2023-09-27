e85c6ac806c43298cf822b37b132c7b381e36643
Guide Fari <guide.fari@flexperto.com>
2023-09-27 11:34:40 +0200

    refactor: clean up prefix stuff. use named return value

--- hello.go
+++ hello.go
@@ -1,14 +1,35 @@
 package main
 
-import "fmt"
+const (
+	french             = "French"
+	spanish            = "Spanish"
+	englishHelloPrefix = "Hello, "
+	spanishHelloPrefix = "Hola, "
+	frenchHelloPrefix  = "Bonjour, "
+)
 
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	return fmt.Sprintf("Hello, %s", name)
+
+	return greetingPrefix(language) + name
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	return
+}
+
+func greetingPrefix(language string) (prefix string) {
+
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+
+	return
 }
