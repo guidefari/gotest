7aaedc4d2b53fecac04ccce00500ac08baa389cf
Guide Fari <guideg6@gmail.com>
2024-05-29 10:54:35 +0200

    refactor: switch to range

--- arraysAndSlices/sum.go
+++ arraysAndSlices/sum.go
@@ -4,8 +4,10 @@ func Sum(numbers []int) int {
 
 	sum := 0
 
-	for i := 0; i < len(numbers); i++ {
-		sum += numbers[i]
+	// range lets you iterate over an array. On each iteration, range returns two values - the index and the value.
+	// We are choosing to ignore the index value by using _ blank identifier.
+	for _, number := range numbers {
+		sum += number
 	}
 
 	return sum
