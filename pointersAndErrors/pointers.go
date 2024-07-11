11c55138032a2ec9a8e64783e08f099bc163e9fe
Guide Fari <guide.fari@shoprite.co.za>
2024-07-11 12:16:45 +0200

    feat: named type - to present domain specific value

--- pointersAndErrors/pointers.go
+++ pointersAndErrors/pointers.go
@@ -1,13 +1,17 @@
 package pointersanderrors
 
+// By doing this we're making a new type and we can declare methods on them.
+// This can be very useful when you want to add some domain specific functionality on top of existing types.
+type Bitcoin int
+
 type Wallet struct {
-	balance int
+	balance Bitcoin
 }
 
-func (w *Wallet) Deposit(amount int) {
+func (w *Wallet) Deposit(amount Bitcoin) {
 	w.balance += amount
 }
 
-func (w *Wallet) Balance() int {
+func (w *Wallet) Balance() Bitcoin {
 	return w.balance
 }
